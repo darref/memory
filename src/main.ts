be650ac127ddb3578e8b0542b425301249725441
myriam <conceptmyriam@outlook.fr>
2023-08-17 15:32:46 +0200

    debut

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,13 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+ const app = document.createElement('div')
+ app.setAttribute('id', "app")
+ const body = document.querySelector('body')
+ body?.appendChild(app)
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+ const memorygame = document.createElement('div')
+ memorygame.classList.add("memory-game")
+ body?.appendChild(memorygame)
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+ const card1 = document.createElement('div')
+ card1.classList.add("card")
+ card1.setAttribute('data-card', '1')
+ card1.style.backgroundColor = ('red')
