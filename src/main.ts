28889b676a6f902aec759c6fd814d6877c2ac19c
Darref <jordan.ferrad@gmail.com>
2023-08-18 15:44:26 +0200

    almost finished

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,17 @@
+let toggleClick = false;
+let previousTileImageClicked = "";
+let previousTileClicked:HTMLDivElement ;
+let nbCoups = 0;
+let voidDiv:HTMLDivElement ;
+
+function getRandomInt(max:number) {
+    return Math.floor(Math.random() * max);
+  }
+
 function loadGame()
 {
     //reset
+    nbCoups = 0;
     document.body.innerHTML = "";
     //fond d ecran abstract
     document.body.style.backgroundImage = "url(memoryBackground.jpg)";
@@ -30,19 +41,95 @@ function loadGame()
     document.body.appendChild(banner);
     document.body.appendChild(gameGrid);
 
+    //tableau de tiles
+    let allTiles:Array<HTMLDivElement> = [];
     //creation et ajout des tuiles
     for(let i=0 ; i<16 ; i++)
     {
-        const element = document.createElement('div');
-        element.id = "card n " + i;
-        element.style.width = "25%";
-        element.style.height = "25%";
-        element.style.display="flex";
-        element.style.backgroundColor = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
-        gameGrid.appendChild(element);
+        const tile = document.createElement('div');
+        tile.id = "card n " + i;
+        tile.style.width = "25%";
+        tile.style.height = "25%";
+        tile.style.display="flex";
+        tile.style.backgroundColor = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+        gameGrid.appendChild(tile);
+        //ajouter la tuile au tableau
+        allTiles.push(tile);
+        //tableau d images
+        
     }
-    
+
+    // some variables ans arrays
+    let allImages:Array<string> = [];
+    let image:string = '';
+    //recupération des images aleatoires
+    for (let i = 0 ; i < 8 ; i++)
+    {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(response => response.json())
+        .then(data => 
+        {
+            // Maintenant, 'data' contient les données JSON de la réponse
+            // Vous pouvez accéder aux URL comme ceci :
+            image = data.message;
+            console.log(image);
+            //choix aleatoire
+            let randomIndice = getRandomInt(8);
+            //remplissage de l image
+
+            allImages[i] = "url("+image+")";
+            allTiles[i].style.backgroundSize = "cover";
+            allTiles[i].style.backgroundRepeat = "no-repeat";
+            allImages[i+8] = "url("+image+")";
+            allTiles[i+8].style.backgroundSize = "cover";
+            allTiles[i+8].style.backgroundRepeat = "no-repeat";
+            
+        })
+        .catch(error => 
+        {
+            console.error("Une erreur s'est produite : ", error);
+        })
+    }
+
+    allTiles.forEach((e, i )=>
+    {
+        e.addEventListener("click" , () =>
+        {
+
+
+            if(!toggleClick) // si on click premiere fois
+            {
+                e.style.backgroundImage = allImages[i];
+                toggleClick = true;
+                previousTileImageClicked = allImages[i];
+                previousTileClicked = e;
+            }
+            else // sinon c'est la deuxieme fois donc on verifie le resultat
+            {
+                if(allImages[i] != previousTileImageClicked) // si on a pas trouvé la paire
+                {
+                    nbCoups++;
+                    e.style.backgroundImage = ""; // on reinitialise les deux images cliquées
+                    previousTileClicked.style.backgroundImage = "";
+                }
+                else // sinon c'est qu on a trouvé la paire 
+                {
+                    nbCoups++;
+                    e.style.backgroundImage = allImages[i];
+                }
+                toggleClick = false;
+                previousTileClicked = voidDiv;
+            }
+        });
+    });
 }
+
+
+    
+    
+    
+    
+
 
 loadGame();
 loadGame();
